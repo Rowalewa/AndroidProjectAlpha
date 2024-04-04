df4924b8dece1734d786160d63785dae3957751c
lesliewanjala06@gmail.com <lesliewanjala06@gmail.com>
2024-04-04 17:39:46 +0300

    update

--- app/src/main/java/com/example/androidprojectone/BottomAppBarActivity.kt
+++ app/src/main/java/com/example/androidprojectone/BottomAppBarActivity.kt
@@ -3,11 +3,14 @@ package com.example.androidprojectone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.LocationOn
@@ -24,6 +27,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidprojectone.ui.theme.AndroidProjectOneTheme
 
@@ -60,18 +64,28 @@ fun BottomAppBar() {
                         Icon(
                             Icons.Filled.Edit,
                             contentDescription = "Localized description",
+                            tint = Color.Yellow
                         )
                     }
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
-                            Icons.Filled.LocationOn,
+                            Icons.Filled.Check,
                             contentDescription = "Localized description",
+                            tint = Color.Blue
                         )
                     }
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
                             Icons.Filled.Notifications,
                             contentDescription = "Localized description",
+                            tint = Color.Red
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Call,
+                            contentDescription = "Localized description",
+                            tint = Color.Red
                         )
                     }
                 },
@@ -81,7 +95,9 @@ fun BottomAppBar() {
                         containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
                         elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
                     ) {
-                        Icon(Icons.Filled.Add, "Localized description")
+                        Icon(Icons.Filled.Add,
+                            "Localized description",
+                            modifier = Modifier.background(color = Color.Cyan))
                     }
                 }
             )
@@ -93,7 +109,10 @@ fun BottomAppBar() {
         )
     }
 }
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Bottom Bar App")
 @Composable
 fun BottomAppBarPreview() {
     AndroidProjectOneTheme {
--- app/src/main/java/com/example/androidprojectone/WhatsAppActivity.kt
+++ app/src/main/java/com/example/androidprojectone/WhatsAppActivity.kt
@@ -11,18 +11,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -54,40 +64,65 @@ class WhatsAppActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WhatsApp(){
-    val context = LocalContext.current
-    TopAppBar(title = { Text(text = "WhatsApp",
-        modifier = Modifier
-            .fillMaxWidth()
-    )},
-        navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Filled.Home, contentDescription = "logo")
-
-            }
+fun WhatsApp() {
+    Scaffold(
+        bottomBar = {
+            androidx.compose.material3.BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(Icons.Filled.Check, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Edit,
+                            contentDescription = "Localized description",
+                            tint = Color.Yellow
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.LocationOn,
+                            contentDescription = "Localized description",
+                            tint = Color.Blue
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Notifications,
+                            contentDescription = "Localized description",
+                            tint = Color.Red
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            Icons.Filled.Done,
+                            contentDescription = "Localized description",
+                            tint = Color.Red
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { /* do something */ },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                    ) {
+                        Icon(
+                            Icons.Filled.Add,
+                            "Localized description",
+                            modifier = Modifier.background(color = Color.Cyan)
+                        )
+                    }
+                }
+            )
         },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Cyan,
-            titleContentColor = Color.Blue,
-            navigationIconContentColor = Color.White
-        ),
-        actions = {
-            IconButton(onClick = { Toast.makeText(context, "You have clicked a Call bar", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.Call, contentDescription = "Call", tint = Color.White)
-
-            }
-            IconButton(onClick = { Toast.makeText(context, "You have clicked a Menu", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.Search, contentDescription = "Share", tint = Color.White)
-
-            }
-            IconButton(onClick = { Toast.makeText(context, "You have clicked a Menu", Toast.LENGTH_SHORT).show() }) {
-                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Menu", tint = Color.White)
-
-            }
-        })  // the title is the name of the organization for which the app is created
-
+    ) { innerPadding ->
+        Text(
+            modifier = Modifier.padding(innerPadding),
+            text = "Example of a scaffold with a bottom app bar."
+        )
+    }
 }
 
 @Preview(
